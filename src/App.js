69f02f9dacf5988adecca39af4e8a6d06cae9e5e
SOHEE <soheee94@gmail.com>
2023-03-28 12:14:11 +0900

    0328

--- src/App.js
+++ src/App.js
@@ -1,25 +1,81 @@
-import logo from './logo.svg';
-import './App.css';
+import { useRef, useState } from "react";
+import styled from "styled-components";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const LI = styled.li`
+&.on {
+    color: #ddd;
+    text-decoration: line-through;
+}
+`
+
+const App = () => {
+    const [list, setList] = useState([]);
+    const [itm, setItm] = useState({
+        title: ''
+    });
+
+    const num = useRef(1);
+
+    const onChange = e => {
+        const { value, name } = e.target
+        setItm({
+            id: num.current,
+            [name]: value,
+            done: false,
+        })
+    }
+
+    const onClick = () => {
+        if (itm.title.length < 3) {
+            alert('more..')
+            return
+        }
+
+        setList([
+            ...list,
+            itm
+        ]);
+
+        setItm({
+            title: '',
+        })
+
+        num.current++;
+    }
+
+    const onDelete = (id) => {
+        alert('삭제하시겠습니까?')
+        const r = list.filter(it => it.id !== id);
+        setList(r);
+    }
+
+    const onModify = (id) => {
+        console.log(id);
+        const r = list.map(it => it.id === id ? { ...it, done: true } : it);
+        setList(r);
+    }
+
+    console.log(list)
+    return (
+        <div>
+            <ul>
+                {
+                    list.map(it => {
+                        return (
+                            <LI key={it.id} className={it.done ? 'on' : ''}>
+                                {it.title}
+                                <button onClick={() => onDelete(it.id)}>DELETE</button>
+                                <button onClick={() => onModify(it.id)}>DO</button>
+                            </LI>)
+                    })
+                }
+            </ul>
+            <input type="text" name='title' value={itm.title || ''} onChange={onChange} />
+            <button onClick={onClick}>추가</button>
+        </div>
+    )
 }
 
 export default App;
+
+//list.push(4)
